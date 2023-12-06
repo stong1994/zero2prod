e63f6b8a0096d42a7059711b4b41ce40ecc5403c
stong <498327580@qq.com>
2023-12-06 11:31:06 +0800

    opt testing log

--- tests/health_check_test.rs
+++ tests/health_check_test.rs
@@ -1,15 +1,29 @@
-use std::fmt::format;
 use std::net::TcpListener;
+use once_cell::sync::Lazy;
 use sqlx::{Connection, Executor, PgConnection, PgPool};
 use uuid::Uuid;
 use zero2prod::configuration::{DatabaseSettings, get_configuration};
 use zero2prod::startup::run;
+use zero2prod::telemetry::{get_subscriber, init_subscriber};
 
+static TRACING: Lazy<()> = Lazy::new(|| {
+    let default_filter_level = "info".to_string();
+    let subscriber_name = "test".to_string();
+    if std::env::var("TEST_LOG").is_ok() {
+        let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::stdout);
+        init_subscriber(subscriber);
+    } else {
+        let subscriber = get_subscriber(subscriber_name, default_filter_level, std::io::sink);
+        init_subscriber(subscriber);
+    }
+});
 pub struct TestApp {
     pub address: String,
     pub db_pool: PgPool,
 }
 async fn spawn_app() -> TestApp{
+    Lazy::force(&TRACING);
+
     let mut configuration = get_configuration().expect("Failed to read configuration");
     let connection_pool= configure_database(&mut configuration.database).await;
     let lst = TcpListener::bind("127.0.0.1:0")
